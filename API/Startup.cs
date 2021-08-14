0b169ff9f618d8e36194e5ba551e8f6309c503bb
Gonzalo Fernandez <jofera@gmail.com>
2021-08-14 13:35:03 +0200

    Run migrations on startup class

--- API/Startup.cs
+++ API/Startup.cs
@@ -40,13 +40,14 @@ namespace API
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, DataContext dataContext)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
+                dataContext.Database.Migrate();
             }
 
             app.UseHttpsRedirection();
