902d8b98dc021ea0d2a9a2b563d6f7f6d89e3d3c
Gonzalo Fernandez <jofera@gmail.com>
2021-08-14 19:09:40 +0200

    Add seed data

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -10,5 +10,14 @@ namespace API.Data
         }
 
         public DbSet<AppUser> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<AppUser>().HasData(
+                new AppUser { Id = 1, UserName = "Darth Vader" },
+                new AppUser { Id = 2, UserName = "Luke Skywalker" },
+                new AppUser { Id = 3, UserName = "Obi-Wan Kenobi" }
+            );
+        }
     }
 }
