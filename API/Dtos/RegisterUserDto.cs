c15364b495da061e5f913bd235fd3b69b2773c86
Gonzalo Fernandez <jofera@gmail.com>
2021-08-15 18:59:25 +0200

    Add some validations to dto

--- API/Dtos/RegisterUserDto.cs
+++ API/Dtos/RegisterUserDto.cs
@@ -1,8 +1,14 @@
-﻿namespace API.Dtos
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.Dtos
 {
     public class RegisterUserDto
     {
+        [Required]
+        [MinLength(3)]
         public string Username { get; set; }
+
+        [Required]
         public string Password { get; set; }
     }
 }
