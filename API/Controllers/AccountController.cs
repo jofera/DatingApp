be68c884dea1af03a77b608d0b2594edc7766cb3
Gonzalo Fernandez <jofera@gmail.com>
2021-08-15 17:59:21 +0200

    Add controller and dto to register user

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,38 @@
+﻿using API.Data;
+using API.Dtos;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    public class AccountController : BaseApiController
+    {
+        private readonly DataContext _dataContext;
+
+        public AccountController(DataContext context)
+        {
+            _dataContext = context;
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<AppUser>> RegisterUser(RegisterUserDto registerUserDto)
+        {
+            using var hmac = new HMACSHA512();
+
+            var user = new AppUser
+            {
+                UserName = registerUserDto.Username,
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerUserDto.Password)),
+                PasswordSalt = hmac.Key
+            };
+
+            _dataContext.Users.Add(user);
+            await _dataContext.SaveChangesAsync();
+
+            return user;
+        }
+    }
+}
---
+++ API/Dtos/RegisterUserDto.cs
@@ -0,0 +1,8 @@
+﻿namespace API.Dtos
+{
+    public class RegisterUserDto
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
