d4603b17eeae927c15ceb617513c013161958dce
Gonzalo Fernandez <jofera@gmail.com>
2021-08-15 17:14:49 +0200

    Create base controller

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -7,9 +7,7 @@ using System.Threading.Tasks;
 
 namespace API.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class UsersController : ControllerBase
+    public class UsersController : BaseApiController
     {
         private readonly DataContext _context;
 
