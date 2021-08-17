e22b2cadaad4043f62e35a9b05bf2feab572c813
Gonzalo Fernandez <jofera@gmail.com>
2021-08-17 11:06:32 +0200

    Remove slash trail

--- API/Startup.cs
+++ API/Startup.cs
@@ -46,7 +46,7 @@ namespace API
 
             app.UseRouting();
 
-            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200", "https://dateappclient.azurewebsites.net/"));
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200", "https://dateappclient.azurewebsites.net"));
 
             app.UseAuthentication();
 
