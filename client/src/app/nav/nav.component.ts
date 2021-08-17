3429fda42de0444fb9075487e86658149d2f656b
Gonzalo Fernandez <jofera@gmail.com>
2021-08-17 21:14:37 +0200

    Implement toastr for notifications

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { Observable } from 'rxjs';
 import { User } from '../_models/user';
 import { AccountService } from '../_services/account.service';
@@ -13,7 +14,9 @@ export class NavComponent implements OnInit {
 
   model: any = {}
 
-  constructor(public accountService: AccountService, private router: Router) { }
+  constructor(public accountService: AccountService, 
+              private router: Router, 
+              private toastr: ToastrService) { }
 
   ngOnInit(): void {
   }
@@ -23,7 +26,7 @@ export class NavComponent implements OnInit {
       console.log(response);
       this.router.navigateByUrl('/members');
     }, error => {
-      console.log(error);
+      this.toastr.error(error.error);
     });
   }
 
