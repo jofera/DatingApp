1750404fcc159627e05fca55a17a0d0bda5d15f9
Gonzalo Fernandez <jofera@gmail.com>
2021-08-19 14:05:25 +0200

    Adjust error interception for HTTP2

--- client/src/app/_interceptors/error.interceptor.ts
+++ client/src/app/_interceptors/error.interceptor.ts
@@ -30,11 +30,11 @@ export class ErrorInterceptor implements HttpInterceptor {
                 }
                 throw modelStateErrors;
                 }else{
-                  this.toastr.error(error.statusText, error.status);
+                  this.toastr.error(error.error, error.status);
                 }
                 break;
             case 401:
-              this.toastr.error(error.statusText, error.status);
+              this.toastr.error(error.error === null ? "Unauthorized" : error.error, error.status);
               break;
             case 404:
               this.router.navigateByUrl('/not-found');
