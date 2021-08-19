7eaa76f17e002c1b4fe97b48511d04ab010c17f3
Gonzalo Fernandez <jofera@gmail.com>
2021-08-19 15:17:41 +0200

    Add not found component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -17,6 +17,7 @@ import { MessagesComponent } from './messages/messages.component';
 import { SharedModule } from '_modules/shared.module';
 import { TestErrorsComponent } from './errors/test-errors/test-errors.component';
 import { ErrorInterceptor } from './_interceptors/error.interceptor';
+import { NotFoundComponent } from './errors/not-found/not-found.component';
 
 @NgModule({
   declarations: [
@@ -28,7 +29,8 @@ import { ErrorInterceptor } from './_interceptors/error.interceptor';
     MemberDetailComponent,
     ListsComponent,
     MessagesComponent,
-    TestErrorsComponent
+    TestErrorsComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
